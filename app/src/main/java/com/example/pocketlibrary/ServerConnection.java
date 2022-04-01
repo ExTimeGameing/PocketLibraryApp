edeb64c78a41ddac2c308bd4f9ee444822c32a7a
Egor <bugrov102002@yandex.ru>
2022-04-01 23:31:56 +0300

    First commit

---
+++ app/src/main/java/com/example/pocketlibrary/ServerConnection.java
@@ -0,0 +1,15 @@
+package com.example.pocketlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServerConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_connection);
+    }
+}
