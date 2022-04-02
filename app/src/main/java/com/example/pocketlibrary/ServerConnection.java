414affe87ef84b16afdc41173d316379f4e50297
Egor <bugrov102002@yandex.ru>
2022-04-02 14:03:39 +0300

    Make Sign Up and Log In

--- app/src/main/java/com/example/pocketlibrary/ServerConnection.java
+++ app/src/main/java/com/example/pocketlibrary/ServerConnection.java
@@ -2,14 +2,19 @@ package com.example.pocketlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ServerConnection extends AppCompatActivity {
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_connection);
-    }
+           }
+
 }
