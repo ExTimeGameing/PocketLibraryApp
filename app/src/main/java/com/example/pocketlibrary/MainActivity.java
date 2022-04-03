177172c119f37207dcb42a654f77f773adef391f
Egor <bugrov102002@yandex.ru>
2022-04-03 13:17:34 +0300

    Create news Activity

--- app/src/main/java/com/example/pocketlibrary/MainActivity.java
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.pocketlibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
