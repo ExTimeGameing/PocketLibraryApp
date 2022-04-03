12317b3b27909f7941702950ef08bac2abed485e
Egor <bugrov102002@yandex.ru>
2022-04-03 17:26:00 +0300

    Create "Map" Activity

--- app/src/main/java/com/example/pocketlibrary/MainActivity.java
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.pocketlibrary;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
