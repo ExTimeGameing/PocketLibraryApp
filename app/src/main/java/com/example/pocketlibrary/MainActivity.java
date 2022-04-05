0adac7d9ac561f620fc4c5f7682ed7da57bba719
Egor <bugrov102002@yandex.ru>
2022-04-05 19:07:11 +0300

    Create "BottomMenu" and transit system

--- app/src/main/java/com/example/pocketlibrary/MainActivity.java
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.java
@@ -1,19 +1,55 @@
 package com.example.pocketlibrary;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.actualNews:
+                    loadFragment(ActualNews.newInstance());
+                    return true;
+                case R.id.booksList:
+                    loadFragment(BooksList.newInstance());
+                    return true;
+                case R.id.profile2:
+                    loadFragment(profile.newInstance());
+                    return true;
+                case R.id.search:
+                    loadFragment(search.newInstance());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentContainerView, fragment);
+        ft.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavig);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
--- app/src/main/java/com/example/pocketlibrary/search.java
+++ app/src/main/java/com/example/pocketlibrary/search.java
@@ -1,15 +1,27 @@
 package com.example.pocketlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class search extends AppCompatActivity {
+public class search extends Fragment {
+    public search(){
+    }
+
+    public static search newInstance() {
+        return new search();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_search, container, false);
     }
 
 
